75080077ae22f1ac8242f045b3d932b16292f79f
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-04-24 17:28:29 -0300

    staff

--- src/controllers/staffControllers.js
+++ src/controllers/staffControllers.js
@@ -1,6 +1,6 @@
 let controller = {
     index: (req, res) => {
-        res.render('staff/employersRead', { title: 'STAFF' })
+        res.render('staff/staff_00', { title: 'STAFF' })
     },
     staff01: (req, res) => {
         res.render('staff/staff_05', { title: 'STAFF' })
