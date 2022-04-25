7c9936b2fdea66c2ab0c866f34a3f8c29d005a4c
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-04-25 01:15:26 -0300

    D

--- src/routes/staffRoutes.js
+++ src/routes/staffRoutes.js
@@ -2,8 +2,8 @@ const staffController = require('./../controllers/staffControllers.js')
 const express = require('express');
 const router = express.Router();
 
-router.get('/', staffController.index)
-router.get('/01', staffController.staff01)
+router.get('/:grL/:id', staffController.index)
+router.get('/', staffController.staff01)
 router.get('/reservas/:idReserve', staffController.getByReserve)
-router.get('/cadastro/:idStaff', staffController.createID);
+router.get('/cadastro', staffController.createID);
 module.exports = router
